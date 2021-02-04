b8464d231a5cb6dc84b2350813024c75da7df431
Orcha02 <orcha2002@gmail.com>
2021-02-04 02:03:47 +0000

    Edit 4-puts.c command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/** 
-*main: entry point
+/**
+* main - entry point
 *
 *
-*return:0 success
+* Return: 0 success
 */
-int main (void)
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
