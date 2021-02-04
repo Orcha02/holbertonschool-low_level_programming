e711940ec5d6f48c6a89a38648053a0f3d2efcdf
Orcha02 <orcha2002@gmail.com>
2021-02-04 22:01:31 +0000

    Fixed betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - fetch last digit of n
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n % 10 > 5)
 	{
 		printf("%d is greater than 5\n", n);
