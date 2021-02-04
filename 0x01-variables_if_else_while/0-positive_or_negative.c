e8a311d55b6644e6f0e399dea5dd114b27827444
Orcha02 <orcha2002@gmail.com>
2021-02-04 21:55:40 +0000

    Simplify command

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,17 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
-	if (n < 0)
-	{
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	}
-	if (n == 0)
-	{
+	else
 		printf("%d is zero\n", n);
-	}
-
 	return (0);
 }
