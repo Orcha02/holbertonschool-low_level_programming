9144463bc39a325a9c7b4496b81b283c1c7af291
Orcha02 <orcha2002@gmail.com>
2021-02-04 02:05:47 +0000

    Edit 5-printf.c command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+
+	return 0;
+}
