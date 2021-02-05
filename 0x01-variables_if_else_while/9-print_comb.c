8129555661a40da55320a434a239f6d355fe1f76
Orcha02 <orcha2002@gmail.com>
2021-02-05 04:33:19 +0000

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int singledigit;
+
+	for (singledigit = '0'; singledigit <= '9'; singledigit++)
+	{
+		putchar(singledigit);
+		if (!(singledigit == '9'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('n');
+	return (0);
+}
