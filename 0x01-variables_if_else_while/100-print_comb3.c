76b1a5b4ba4b8fcd895f87153c44ed9ef90eb895
Orcha02 <orcha2002@gmail.com>
2021-02-05 21:27:38 +0000

    Add advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
+		for (digit2 == digit1 + 1); digit2 <= '9'; digit2++)
+{
+	putchar(digit1);
+	putchar(digit2);
+	if (!(digit1 == '8'
+}
+return (0);
+}
