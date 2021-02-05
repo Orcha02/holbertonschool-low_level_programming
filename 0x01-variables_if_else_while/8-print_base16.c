90718f271fc0bdde0c587ac027a6944be855c421
Orcha02 <orcha2002@gmail.com>
2021-02-05 03:55:19 +0000

    Add 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@ int main(void)
 {
 	char num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		printf("%x", num);
+		putchar(num);
+	}
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
 	}
 	putchar ('\n');
 	return (0);
