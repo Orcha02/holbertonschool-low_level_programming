a6b916d9da21c1e462411ffe1c96c23692b2ea40
Orcha02 <orcha2002@gmail.com>
2021-02-05 03:33:55 +0000

    Add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case
+ *
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%x", num);
+	}
+	putchar ('\n');
+	return (0);
+}
