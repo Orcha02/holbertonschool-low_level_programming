b2e045bef7929dc9ea8c4913a0637565e0a4b3f5
Orcha02 <orcha2002@gmail.com>
2021-02-05 04:49:53 +0000

    Fixed betty

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -15,7 +15,7 @@ int main(void)
 		{
 			putchar(digit1);
 			putchar(digit2);
-			if(!(digit1 == '9' && digit2 == '9'))
+			if (!(digit1 == '9' && digit2 == '9'))
 			{
 				putchar(',');
 				putchar(' ');
