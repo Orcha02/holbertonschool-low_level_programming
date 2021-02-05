6885a0a1c24c20dbc8d27ead97a8991f6006b60f
Orcha02 <orcha2002@gmail.com>
2021-02-05 04:48:11 +0000

    Add 10-print.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 00 to 99
+ *
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
+		for (digit2 = '0'; digit2 <= '9'; digit2++)
+		{
+			putchar(digit1);
+			putchar(digit2);
+			if(!(digit1 == '9' && digit2 == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
