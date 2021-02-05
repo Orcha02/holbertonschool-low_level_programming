5362cc14ce05d441ac92238a9911ca0fefdf2406
Orcha02 <orcha2002@gmail.com>
2021-02-05 02:24:27 +0000

    Add 6-print-numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers starting from 0, followed by a new line.
+ *
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num ++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
