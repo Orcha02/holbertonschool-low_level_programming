797404ab722b15f59c2161287a6877265920f56e
Orcha02 <orcha2002@gmail.com>
2021-02-08 23:06:04 +0000

    Add 0-holberton.c holberton.h

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("Holberton\n");
+	puts("Holberton");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON-HEADER
+#define HOLBERTON-HEADER
+int _putchar(char c)
+
+#endif
