2b563eff2ca0534d928a1e70ebac782b9dcb0b59
Orcha02 <orcha2002@gmail.com>
2021-02-09 22:09:49 +0000

    Add 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int row, col, mult;
+	short int uni, dec;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			mult = row * col;
+			uni = mult % 10;
+			dec = mult / 10;
+			if (dec == 0)
+				_putchar(' ');
+			else
+				_putchar (dec + '0');
+			_putchar (uni + '0');
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
