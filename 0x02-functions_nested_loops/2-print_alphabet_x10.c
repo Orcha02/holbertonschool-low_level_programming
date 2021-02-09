3715cb53963e86c6f55a513a6bda1952fa722093
Orcha02 <orcha2002@gmail.com>
2021-02-09 15:25:45 +0000

    Edit of docu

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * main - prints 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints 10 times lowercase alphabetand then a new line.
  *
  *
  * Return: 0 success
