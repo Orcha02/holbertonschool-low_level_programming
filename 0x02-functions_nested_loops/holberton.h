c772d5a6d91135396b9d60f56a7b31ff989d98ae
Orcha02 <orcha2002@gmail.com>
2021-02-09 14:14:52 +0000

    Edit holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,24 @@
-#ifndef HOLBERTON-HEADER
-#define HOLBERTON-HEADER
-int _putchar(char c)
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+/*prints a char stdout*/
+int _putchar(char c);
+
+/*prints alphabet a lowercase*/
+void print_alphabet(void);
+
+/*prints alphabet 10 times*/
+void print_alphabet_x10(void);
+
+/*function that checks for lowercase character.*/
+int _islower(int c);
+
+/*function that checks for alphabetic character.*/
+int _isalpha(int c);
+
+/*function that prints the sign of a number.*/
+int print_sign(int n);
+
+/*function that computes the absolute value of an integer.*/
+int _abs(int);
 #endif
