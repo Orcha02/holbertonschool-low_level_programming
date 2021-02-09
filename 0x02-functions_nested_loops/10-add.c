8a5274f84868e243d17d1302440d461f3858739c
Orcha02 <orcha2002@gmail.com>
2021-02-09 20:50:13 +0000

    Add 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * add - check the code for Holberton School students.
+ *
+ *@a: int
+ *
+ *@b: int
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int suma;
+
+	suma = (a + b);
+	return (suma);
+}
