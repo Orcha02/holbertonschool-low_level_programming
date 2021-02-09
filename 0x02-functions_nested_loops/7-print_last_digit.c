8b266e309bf94d7c3b5de76cd2525e26e66ab3c7
Orcha02 <orcha2002@gmail.com>
2021-02-09 18:21:43 +0000

    Add 7-print_last_digit_absolute

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_last_digit - function that prints the last digit of a number.
+ * @i: int
+ *
+ * Return: 0 success
+ */
+int print_last_digit(int i)
+{
+	int lastdigit;
+	lastdigit = (i % 10);
+	if (lastdigit < 0)
+	{
+		_putchar((-1 * lastdigit) + '0');
+		return (-1 * lastdigit);
+	}
+	else
+	{
+		_putchar((lastdigit) + '0');
+		return (lastdigit);
+	}
+}
