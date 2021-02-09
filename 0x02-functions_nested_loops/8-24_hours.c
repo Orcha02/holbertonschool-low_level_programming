ce95aabaa2d5c07821fd4fc6c069f686a3d23c60
Orcha02 <orcha2002@gmail.com>
2021-02-09 19:45:22 +0000

    Edit < to <=

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ void jack_bauer(void)
 	int num3;
 	int num4;
 
-	for (num1 = '0'; num1 < '2'; num1++)
-		for (num2 = '0'; num2 < '3'; num2++)
-			for (num3 = '0'; num3 < '5'; num3++)
-				for (num4 = '0'; num4 < '9'; num4++)
+	for (num1 = '0'; num1 <= '2'; num1++)
+		for (num2 = '0'; num2 <= '3'; num2++)
+			for (num3 = '0'; num3 <= '5'; num3++)
+				for (num4 = '0'; num4 <= '9'; num4++)
 				{
 					if (!(num1 == '2' && num2 == '4'))
 					{
