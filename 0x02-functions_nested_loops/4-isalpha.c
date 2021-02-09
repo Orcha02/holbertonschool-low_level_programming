81eeb3e199a65bb52ea0172c551f661680621a2c
Orcha02 <orcha2002@gmail.com>
2021-02-09 15:41:04 +0000

    Edit of docu

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
-/*
-* main -
-*
+/**
+* _isalpha - function that checks for alphabetic character.
+* @c: external input
 *
 * Return: 0 success
 */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
