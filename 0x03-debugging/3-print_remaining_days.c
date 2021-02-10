8a53d06e6b091b1c684720aad34044a706924ec2
Orcha02 <orcha2002@gmail.com>
2021-02-10 02:31:50 +0000

    Edit of command

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+0#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 400 == 0) || (year % 400 == 0) && !(year % 100 == 0)))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
