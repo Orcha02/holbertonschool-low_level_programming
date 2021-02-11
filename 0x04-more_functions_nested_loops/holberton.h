6b1ca3355beeece3fa737e28bce7e04789361163
Orcha02 <orcha2002@gmail.com>
2021-02-11 19:06:42 +0000

    Modified .h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,10 +1,13 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+/*prints a char stdout*/
+int _putchar(char c);
 /*function that checks for uppercase character.*/
 int _isupper(int c);
 /*function that checks for a digit (0 through 9).*/
 int _isdigit(int c);
 /*function that multiplies two integers.*/
 int mul(int a, int b);
+/*prints the numbers, from 0 to 9, followed by a new line.*/
+void print_numbers(void);
 #endif
