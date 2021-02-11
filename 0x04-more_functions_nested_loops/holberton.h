3dbf03f66e79e02f09a4684593e9a4dac5ea56c7
Orcha02 <orcha2002@gmail.com>
2021-02-11 18:07:46 +0000

    Add 0-isupper .h file and README

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*function that checks for uppercase character.*/
+int _isupper(int c);
+/*function that checks for a digit (0 through 9).*/
+int _isdigit(int c);
+#endif
