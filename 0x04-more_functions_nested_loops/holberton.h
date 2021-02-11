567156fd9396895596ed09d0df2dc12767bca101
Orcha02 <orcha2002@gmail.com>
2021-02-11 22:29:58 +0000

    Edit .h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -12,4 +12,6 @@ int mul(int a, int b);
 void print_numbers(void);
 /*prints the numbers, from 0 to 9, followed by a new line.*/
 void print_most_numbers(void);
+/*prints 10 times the numbers, from 0 to 14, followed by a new line.*/
+void more_numbers(void);
 #endif
