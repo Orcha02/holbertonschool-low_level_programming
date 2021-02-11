c24a42d41099037883fbdf170c57496afdb5cc8b
Orcha02 <orcha2002@gmail.com>
2021-02-11 22:29:15 +0000

    Add 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14 and a new line
+ *
+ *
+ * Return: 0 Success
+ */
+void more_numbers(void)
+{
+	int n, times;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n %10) + '0');
+		}
+	_putchar('\n');
+	}
+}
