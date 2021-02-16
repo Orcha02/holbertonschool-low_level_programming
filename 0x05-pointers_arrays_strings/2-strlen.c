7d5a80aa7a477a49ae20bf6f59f0f2037fc1bc1b
Orcha02 <orcha2002@gmail.com>
2021-02-16 15:43:45 +0000

    Comments

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,7 +10,18 @@ int _strlen(char *s)
 {
 	int charCount;
 
+	while (*s != '\0')
+	{
+		charCount++;
+		s++;
+	}
+	return (charCount);
+}
+
+/**(It can also be done with for:
+
 	for (charCount = 0; s[charCount] != '\0'; charCount++)
 	{}
 	return (charCount);
+*/
 }
