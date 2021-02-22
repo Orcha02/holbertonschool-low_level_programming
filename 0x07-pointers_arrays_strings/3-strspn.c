e519734837a93c6744e59142174ba24964934d87
Orcha02 <orcha2002@gmail.com>
2021-02-22 23:25:45 +0000

    Edit command

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != s[i]; j++)
 		{
 			if (accept[j] == '\0')
 				return (i);
