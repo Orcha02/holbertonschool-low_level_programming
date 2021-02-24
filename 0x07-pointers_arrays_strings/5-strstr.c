daf499ab2735316eb48a0eb9f20685a26ea8b521
Orcha02 <orcha2002@gmail.com>
2021-02-24 01:20:42 +0000

    Edit of 5 command

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,39 +7,22 @@
  *Description: func finds first occurrence of substring needle in string hay
  * Return:char pointer
  **/
-int _strcmp(char *s1, char *ned)
-{
-	int i = 0;
-/* First I compared both strings*/
-	while (s1[i] == ned[i])
-	{
-		if (ned[i + 1] == '\0' && s1[i + 1] != '\0')
-			return(0);
-		if (s1[i] == '\0' && ned[i] == '\0')
-			return (0);
-		i++;
-	}
-
-	return (s1[i] - ned[i]);
-}
-
 char *_strstr(char *haystack, char *needle)
 {
-/*Function to find first occuerence of the subtring*/
-	char *h = haystack;
-	char *n = needle;
 
-	if (needle == NULL)
-		return (NULL);
-	while (h != '\0')
+	while (*haystack != '\0')
 	{
-		if (*h == *n)
-		{
-			if (_strcmp(h, n) == 0)
+		char *i = haystack;
+		char *j = needle;
 
-				return (h);
+		while (*j == *haystack && *j != '\0' && *haystack != '\0')
+		{
+			haystack++;
+			j++;
 		}
-		h++;
+		if (*j == '\0')
+			return (i);
+		haystack = i + 1;
 	}
-	return ('\0');
+	return (0);
 }
