8fe9c78094b4c93f360cecbb7637f003168e8557
Orcha02 <orcha2002@gmail.com>
2021-03-01 23:04:58 +0000

    Edit of code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: Array of arguments.
  * Return: 0 is succes.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,4 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int main(int argc, char *argv[]);
 int main(int argc, char **argv);
 #endif
