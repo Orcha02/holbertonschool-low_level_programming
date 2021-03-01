e35a4e47026360292ba75f7e77e58adaaf58d512
Orcha02 <orcha2002@gmail.com>
2021-03-01 23:19:40 +0000

    Add 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it receives.
+ * @argc:  number of command lines
+ * @argv: array containing command lines
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
