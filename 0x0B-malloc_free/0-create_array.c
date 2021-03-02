d06e89dc98ed111f41a326db25e90bc4802e90a1
Orcha02 <orcha2002@gmail.com>
2021-03-02 21:46:08 +0000

    Add p == NULL

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	p = malloc(size * sizeof(int));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	if (size != 0)
 	{
 		for (i = 0; i < size; i++)
