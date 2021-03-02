7da329b149523521b4aa7f8a252be785ca024feb
Orcha02 <orcha2002@gmail.com>
2021-03-02 21:19:24 +0000

    Add 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * create_array - creates an array of chars
+ * initializes it with a specific char.
+ *
+ * @size: unsigned int
+ * @c: char to print
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	p = malloc(size * sizeof(int));
+	if (size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+		*(p + i) = c;/* p[i] = c*/
+		}
+		return (p);
+	}
+	else
+	{
+		return (NULL);
+	}
+	free(p);
+}
