dd8844f2f7e833f1910497a243f2df6a5f42834d
Orcha02 <orcha2002@gmail.com>
2021-03-03 04:28:30 +0000

    Fixed Betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,24 +16,24 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-			array = malloc(width * sizeof(int *));
+	array = malloc(width * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
 	{
-		array[i] = malloc(sizeof(int)* height);
-				  if (array[i] == NULL)
-				  {
-					  return (NULL);
-				  }
+		array[i] = malloc(sizeof(int) * height);
+		if (array[i] == NULL)
+		{
+			return (NULL);
+		}
 	}
 /*Llenamos el array*/
 	for (i = 0 ; i < width; i++)
-			{
-				for (j = 0; j < height; j++)
-				{
-					array [i][j] = 0;
-				}
-			}
+	{
+		for (j = 0; j < height; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
 	return (array);
 }
