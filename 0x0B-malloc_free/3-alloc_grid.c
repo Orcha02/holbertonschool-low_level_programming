b53ae2cc17caf1f77a527dbe81823bb08e5a9fe1
Orcha02 <orcha2002@gmail.com>
2021-03-03 05:23:02 +0000

    Edit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,11 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @width: int
- * @height: int
- *
- * Return: char pointer
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
  */
 int **alloc_grid(int width, int height)
 {
@@ -16,29 +15,28 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(width * sizeof(int *));
+	array = (int **) malloc(sizeof(int *) * height);
+
 	if (array == NULL)
-	return (NULL);
-	for (i = 0; i < width; i++)
 	{
-		array[i] = malloc(height * sizeof(int));
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
-			for (; i >= 0; j--)
-			{
-				free(array[i]);
-			}
+			for (j = 0; j < i; j++)
+				free(array[j]);
 			free(array);
 			return (NULL);
 		}
 	}
 /*Llenamos el array*/
-	for (i = 0 ; i < width; i++)
-	{
-		for (j = 0; j < height; j++)
-		{
+	for (i = 0; i < height; i++)
+		for (j = 0; j < i; j++)
 			array[i][j] = 0;
-		}
-	}
+
 	return (array);
 }
