16fa7583a6350d3ee2763da48de35f7af4664efb
Orcha02 <orcha2002@gmail.com>
2021-03-03 02:54:37 +0000

    Add 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: double pointer
+ * @height: int
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int c;
+
+	for (c = 0; c < height; c++)
+	{
+		free grid[c];
+	}
+}
