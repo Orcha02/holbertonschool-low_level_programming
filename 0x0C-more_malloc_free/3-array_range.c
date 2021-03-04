2a893495bb1935c135c9caaf760daa6142bae335
Orcha02 <orcha2002@gmail.com>
2021-03-04 23:42:19 +0000

    Fixed betty'

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	array = malloc (sizeof(int) * (max - min + 1));
+	array = malloc(sizeof(int) * (max - min + 1));
 
 	if (array == NULL)
 		return (NULL);
@@ -26,5 +26,5 @@ int *array_range(int min, int max)
 	{
 		array[c] = min;
 	}
-	return(array);
+	return (array);
 }
