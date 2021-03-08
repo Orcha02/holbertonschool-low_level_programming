5e073257608d4ebce7213ebfbed9bd5b53a81977
Orcha02 <orcha2002@gmail.com>
2021-03-08 22:41:53 +0000

    Add 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - main entry
+ * @d: pointer to struct
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d ->name == NULL ? "(nil)" : d->name);
+		printf("Age: %f\n", d ->age);
+		printf("Owner: %s\n", d ->owner == NULL ? "(nil)" : d -> owner);
+	}
+}
