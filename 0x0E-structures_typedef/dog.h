427bd84286e115f8bea86ceb1c823a54315d96b5
Orcha02 <orcha2002@gmail.com>
2021-03-08 21:31:33 +0000

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structur for a dog
+ * @name: type char
+ * @age: type float
+ * @owner: type char
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
