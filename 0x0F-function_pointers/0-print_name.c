447b2af4e09493424c319c4390bbf3abc8d35015
Orcha02 <orcha2002@gmail.com>
2021-03-10 21:31:30 +0000

    Add 0, README and .h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_name - function that prints a name.
+ * @name: char pointer
+ * @f: pointer to a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+        if (name != NULL && f != NULL)
+        {
+                (*f)(name);
+        }
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _FUNC_POINTERS_H
+#define _FUNC_POINTERS_H
+void print_name(char *name, void (*f)(char *));
+#endif /*_FUNC_POINTERS_H*/
