2e555c091e43231ee79c2e78a87a290cab46b3e0
Orcha02 <orcha2002@gmail.com>
2021-03-11 04:56:24 +0000

    Add 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,5 +18,17 @@ int (*get_op_func(char *s))(int, int)
 	  {"%", op_mod};
 	  {NULL, NULL}
 	};
-	int i;
+	int i = 0;
+
+	while (ops[i].op)
+	{
+/*Validamos que la primera posicion q recibimos en op sea igual en string*/ 
+/*Validamos que sea solo un caracter y asegurarnos que el siguiente sea nulo*/ 
+		if (ops[i].op[0] == s[0] == '\0')
+		{
+			return (ops[i].f)
+		}
+	i++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,31 @@
 #include <stdio.h>
 /**
  * main - main entry
- *
- *
+ * @argc: argument count
+ * @argv: argument
  * Return: 0
  */
+int main(int argv, char *argc[])
+/*usamos argv y argc porq no nos deja acceder a la funcion directamente*/
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);/*para indicar que es de error*/
+	}
+	/*recibo el codigo no string o dato CODIGO por ser puntero a una funcion*/
+	p_func = get_op_func(argv[2]);
+
+	if (p_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	r = p_func(x, y);
+
+	printf("%d\n", r);
+	return (0);
+}
