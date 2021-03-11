e1ccaefdb39ce5b97ceef0a42b0dd5a7498792aa
Orcha02 <orcha2002@gmail.com>
2021-03-11 20:08:18 +0000

    Add 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - function that prints strings, followed by a new line
+ * @separator: constant pointer char
+ * @n: constant unsigned int
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list my_list;
+	unsigned int counter;
+	char *words;
+
+	va_start(my_list, n);/*Initializes argument list*/
+
+	for (counter = 0; counter < n; counter++)
+	{
+		words = va_arg(my_list, char *);/*gets next argument value*/
+
+		if (words == NULL)
+		{
+			prinf("(nil)");
+		}
+		else
+			printf("%s", words);
+	}
+	if (counter < n - 1 && separator != NULL)
+	{
+		printf("%s", separator);
+	}
+	va_end(my_list); /*cleans up*/
+	printf("\n");
+}
