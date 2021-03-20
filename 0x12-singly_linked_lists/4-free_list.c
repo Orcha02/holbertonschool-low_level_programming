69740943bfc83724c05911472c942d7846ca9b4e
Orcha02 <orcha2002@gmail.com>
2021-03-20 00:42:13 +0000

    Fixed free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 /**
- *free_list - function that frees a list_t list.
- * @head: pointer to a structure typedef list_t
+ * free_list - function that frees a list_t list.
+ * @h: pointer to a structure typedef list_t
  * Return: void
  */
 void free_list(list_t *head)
@@ -12,8 +11,9 @@ void free_list(list_t *head)
 	if (head != NULL)
 	{
 		if (head->next != NULL)
-			free(head->next);
-	free(head->str);
-	free(head);
+			free_list(head->next);
+
+		free(head->str);
+		free(head);
 	}
 }
