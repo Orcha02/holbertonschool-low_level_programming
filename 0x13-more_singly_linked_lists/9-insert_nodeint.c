bb0990772bf3cedc4c46f657517450f9baadc888
Orcha02 <orcha2002@gmail.com>
2021-03-24 01:16:15 +0000

    Add 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function that adds a new node
+ * in an index position of list
+ * @head: pointer to a pointer
+ * @idx: int that represents the index, where the node is going to be inserted
+ * @n: integer value of the structure
+ * Return: the address of the new element or NULL if fail (the new node)
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *current_node = *head;
+	unsigned int counter = 0;
+
+	new_node = malloc(sizeof(listint_t));/*Memory for new_node*/
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+/*Assign head to new_node->next*/
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	while (counter != (idx - 1))
+	{
+		if (current_node == NULL)
+		{
+			free(current_node);
+			return (NULL);
+		}
+/*Assign next address of current_node to current_node*/
+		current_node = current_node->next;/*current_node = &next*/
+	}
+/*Assign new_node address to current_node*/
+	new_node->next = new_node->next;
+	current_node->next = new_node;
+	return (new_node);
+}
