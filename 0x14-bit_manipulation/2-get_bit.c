7782647c90ec0b1c8249fe9f5d9bf25276a56882
Orcha02 <orcha2002@gmail.com>
2021-03-26 02:11:15 +0000

    Add 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index.
+ * @n: int number
+ * @index: is the index, starting from 0 of the bit you want to get
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+/*Validate if index is greater than size of bits*/
+	if (index > 64)
+		return (-1);
+/*Right shift the index times and compare it with 1*/
+	return ((n >> index) & 1);
+}
