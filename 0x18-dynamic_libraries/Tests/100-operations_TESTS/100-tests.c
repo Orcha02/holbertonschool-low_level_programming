e0111fcfb0581577a19c05857aff87f8acbe7a39
Orcha02 <orcha2002@gmail.com>
2021-05-03 23:26:52 +0000

    Add 100-operations.so

---
+++ 0x18-dynamic_libraries/Tests/100-operations_TESTS/100-tests.c
@@ -0,0 +1,70 @@
+#include "headers.h"
+/**
+ * add - Adds a + b
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+
+/**
+ * sub - Substraction between a - b
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int sub(int a, int b)
+{
+	int resta;
+
+	resta = a - b;
+	return (resta);
+}
+
+/**
+ * mul - Multiplies a * b
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+
+/**
+ * div - Divides a / b
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int div(int a, int b)
+{
+	int div;
+
+	div = a / b;
+	return (div);
+}
+
+/**
+ * mod - Module of  a % b
+ * @a: int
+ * @b: int
+ * Return: Always 0 (Success)
+ */
+int mod(int a, int b)
+{
+	int modul;
+
+	modul = a + b;
+	return (modul);
+}
