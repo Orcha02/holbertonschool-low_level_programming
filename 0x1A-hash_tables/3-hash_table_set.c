30ddce3d2f39706418a5d6958d3ce4e2ced977f8
Orcha02 <orcha2002@gmail.com>
2021-05-28 20:35:11 +0000

    Reorganize

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,30 +10,46 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node, *tmp;
-	unsigned int index;
-
-	index = key_index((unsigned char *)key, ht->size); /*Find index*/
-	tmp = ht->array[index];/*Assign head to the hash table index*/
+	unsigned int index = 0;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
+
+	tmp = ht->array[index];/*Assign head to the hash table index*/
+	index = key_index((unsigned char *)key, ht->size); /*Find index*/
+
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		if (strcmp(tmp->key, key) == 0)/*Compare to see if exists*/
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);/*copy value*/
+			if (tmp->value == NULL)
+				return (0);/*0 if it not succeded*/
+			return (1);/*1 if it succeded*/
+		}
+	}
 /*allocate memory for new node*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node != NULL) /* if it is == NULL, will have seg fault*/
-		return (0);/*0 if it not succeded*/
+		return (0);
 
-	if (strcmp(key, tmp->key) == 0)/*Compare to see if exists*/
-	{
-		free(tmp->value);
-		tmp->value = strdup(value);/*copy value*/
-		return (1);/*1 if it succeded*/
-	}
-	tmp = tmp->next;
-/*update value*/
-	new_node->value = strdup(value);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
